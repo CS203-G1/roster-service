95dcdf4e07c118b136623b8e34cce9ccfc4b2010
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 17:06:46 +0800

    src: main: java: csd: roster: controller: CompanyController: Throw CompanyNotFoundException when getAllCompanies return null
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/CompanyController.java
+++ src/main/java/csd/roster/controller/CompanyController.java
@@ -1,11 +1,13 @@
 package csd.roster.controller;
 
+import csd.roster.exception.CompanyNotFoundException;
 import csd.roster.model.Company;
 import csd.roster.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -24,7 +26,8 @@ public class CompanyController {
 
     @GetMapping("/companies/{id}")
     public Company getCompanyById(@PathVariable UUID id) {
-        return companyService.getCompanyById(id);
+        return companyService.getCompanyById(id)
+                .orElseThrow(() -> new CompanyNotFoundException(id));
     }
 
     @GetMapping("/companies")
