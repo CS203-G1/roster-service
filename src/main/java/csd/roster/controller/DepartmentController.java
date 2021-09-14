003ba26707b27b7985e6bf5afb2191a593e12015
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 17:03:06 +0800

    src: main: java: csd: roster: controller: DepartmentController: Implement controller methods to add department
    src: main: java: csd: roster: controller: DepartmentController: Implement controller methods to get department by company and department id
    
    src: main: java: csd: roster: controller: DepartmentController: Implement controller methods to get all departments
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/DepartmentController.java
+++ src/main/java/csd/roster/controller/DepartmentController.java
@@ -1,6 +1,9 @@
 package csd.roster.controller;
 
+import csd.roster.exception.CompanyNotFoundException;
+import csd.roster.exception.DepartmentNotFoundException;
 import csd.roster.model.Department;
+import csd.roster.service.CompanyService;
 import csd.roster.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,20 +14,30 @@ import java.util.UUID;
 @RestController
 public class DepartmentController {
     private DepartmentService departmentService;
+    private CompanyService companyService;
 
     @Autowired
-    public DepartmentController(DepartmentService departmentService) {
+    public DepartmentController(DepartmentService departmentService, CompanyService companyService) {
         this.departmentService = departmentService;
+        this.companyService = companyService;
     }
 
-    @PostMapping("/departments")
-    public Department addDepartment(@RequestBody Department department) {
-        return departmentService.addDepartment(department);
+    @PostMapping("/companies/{companyId}/departments")
+    public Department addDepartment(@PathVariable (value = "companyId") UUID companyId, @RequestBody Department department) {
+        return companyService.getCompanyById(companyId).map(company -> {
+            department.setCompany(company);
+            return departmentService.addDepartment(department);
+        }).orElseThrow(() -> new CompanyNotFoundException(companyId));
+
     }
 
-    @GetMapping("/departments/{id}")
-    public Department getDepartmentById(@PathVariable UUID id) {
-        return departmentService.getDepartmentById(id);
+    @GetMapping("/companies/{companyId}/departments/{id}")
+    public Department getDepartmentById(@PathVariable UUID companyId, @PathVariable UUID departmentId) {
+        if (companyService.getCompanyById(companyId) == null)
+            throw new CompanyNotFoundException(companyId);
+
+        return departmentService.getDepartmentByIdAndCompanyId(departmentId, companyId)
+                .orElseThrow(() -> new DepartmentNotFoundException(companyId, departmentId));
     }
 
     @GetMapping("/departments")
