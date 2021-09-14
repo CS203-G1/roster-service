6a2e32f4bfd235bb7d277911410474ec5b3c9649
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 17:08:26 +0800

    src: main: java: csd: roster: model: Department: Add Lombok Annotations for Department Model
    src: main: java: csd: roster: repository: DepartmentRepository: Add findByIdAndCompanyId method
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/model/Department.java
+++ src/main/java/csd/roster/model/Department.java
@@ -1,9 +1,17 @@
 package csd.roster.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Department {
     @javax.persistence.Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/csd/roster/repository/DepartmentRepository.java
+++ src/main/java/csd/roster/repository/DepartmentRepository.java
@@ -4,8 +4,10 @@ import csd.roster.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, UUID> {
+    Optional<Department> findByIdAndCompanyId(UUID departmentId, UUID companyId);
 }
