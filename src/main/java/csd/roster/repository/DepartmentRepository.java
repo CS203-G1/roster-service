4b821384105d75eff2db23ebc515cb614fb4b32d
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 13:51:57 +0800

    src: main: java: csd: roster: repository: DepartmentRepository: setup JPA Repository for Department Repository
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/DepartmentRepository.java
+++ src/main/java/csd/roster/repository/DepartmentRepository.java
@@ -1,4 +1,11 @@
 package csd.roster.repository;
 
-public interface DepartmentRepository {
+import csd.roster.model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, UUID> {
 }
