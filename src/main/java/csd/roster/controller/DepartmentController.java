354019a6ce244ce9b2866a8a110da0dcb2759c26
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 23:59:05 +0800

    src: main: java: csd: roster: controller: DepartmentController: Add error-handling to handle resources not found errors
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/DepartmentController.java
+++ src/main/java/csd/roster/controller/DepartmentController.java
@@ -5,8 +5,8 @@ import csd.roster.exception.DepartmentNotFoundException;
 import csd.roster.model.Department;
 import csd.roster.service.CompanyService;
 import csd.roster.service.DepartmentService;
-import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,17 +50,26 @@ public class DepartmentController {
     }
 
     @PutMapping("/companies/{companyId}/departments/{departmentId}")
-    public Department updateDepartment(@PathVariable (value = "companyId") UUID companyId,
+    public ResponseEntity<?> updateDepartment(@PathVariable (value = "companyId") UUID companyId,
                                        @PathVariable (value = "departmentId") UUID departmentId,
                                        @RequestBody Department department) {
-        return departmentService.update(companyId, departmentId, department);
+        try {
+            Department updatedDepartment = departmentService.update(companyId, departmentId, department);
+            return new ResponseEntity<>(updatedDepartment, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/companies/{companyId}/departments/{departmentId}")
     public ResponseEntity<?> deleteDepartment(@PathVariable (value = "companyId") UUID companyId,
                                        @PathVariable (value = "departmentId") UUID departmentId) {
 
-        departmentService.delete(companyId, departmentId);
-        return ResponseEntity.ok().build();
+        try {
+            departmentService.delete(companyId, departmentId);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
