307eabd7d4d14e40543604d9b160b545809596a9
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 15:47:46 +0800

    src: main: java: csd: roster: controller: DepartmentController: Add controller methods to add department, get department by id and get all departments
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/DepartmentController.java
+++ src/main/java/csd/roster/controller/DepartmentController.java
@@ -1,4 +1,34 @@
 package csd.roster.controller;
 
+import csd.roster.model.Department;
+import csd.roster.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
 public class DepartmentController {
+    private DepartmentService departmentService;
+
+    @Autowired
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @PostMapping("/departments")
+    public Department addDepartment(@RequestBody Department department) {
+        return departmentService.addDepartment(department);
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department getDepartmentById(@PathVariable UUID id) {
+        return departmentService.getDepartmentById(id);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> getDepartments() {
+        return departmentService.getAllDepartments();
+    }
 }
