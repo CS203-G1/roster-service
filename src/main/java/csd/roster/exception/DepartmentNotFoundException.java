519e6e509d2f9240073f9f941d27ef4133afeb22
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-15 00:31:28 +0800

    src: main: java: csd: roster: exception: ResourceNotFoundException: Add exception parent class to generalize resource not found exceptions
    src: main: java: csd: roster: exception: CompanyNotFoundException: Extend ResourceNotFoundException
    
    src: main: java: csd: roster: exception: DepartmentNotFoundException: Extend ResourceNotFoundException
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/exception/DepartmentNotFoundException.java
+++ src/main/java/csd/roster/exception/DepartmentNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.UUID;
 
 @ResponseStatus(HttpStatus.NOT_FOUND) // 404 Error
-public class DepartmentNotFoundException extends RuntimeException{
+public class DepartmentNotFoundException extends ResourceNotFoundException {
     private static final long serialVersionUID = 1L;
 
     public DepartmentNotFoundException(UUID departmentId, UUID companyId) {
