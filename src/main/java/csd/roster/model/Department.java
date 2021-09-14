003312b3439f83ae323eb4b09b5fe0ca28d4bdbb
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 13:42:25 +0800

    src: model: java: csd: roster: model: Department: Add private fields and add ManyToOne relationship to company
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/model/Department.java
+++ src/main/java/csd/roster/model/Department.java
@@ -1,4 +1,19 @@
 package csd.roster.model;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
 public class Department {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name="company_id")
+    private Company company;
+
+    @Column(name="name")
+    private String name;
 }
