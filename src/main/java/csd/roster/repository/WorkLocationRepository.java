b0fa2c610ab1119981f153738e9e5beab9b5e11b
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-15 00:51:20 +0800

    src: main: java: csd: roster: repository: WorkLocationRepository: Add JPA Repository for WorkLocation
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

---
+++ src/main/java/csd/roster/repository/WorkLocationRepository.java
@@ -0,0 +1,11 @@
+package csd.roster.repository;
+
+import csd.roster.model.WorkLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface WorkLocationRepository extends JpaRepository<WorkLocation, UUID> {
+}
