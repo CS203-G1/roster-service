9e1565330c23e78d992dd66036410d779d1b8c8b
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 18:45:24 +0800

    src: main: java: csd: roster: controller: DepartmentController: Add unimplemented methods updateDepartment and deleteDepartment
    src: main: java: csd: roster: controller: DepartmentController: Breakline at line 27 to reduce line length
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/DepartmentController.java
+++ src/main/java/csd/roster/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import csd.roster.exception.DepartmentNotFoundException;
 import csd.roster.model.Department;
 import csd.roster.service.CompanyService;
 import csd.roster.service.DepartmentService;
+import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +24,8 @@ public class DepartmentController {
     }
 
     @PostMapping("/companies/{companyId}/departments")
-    public Department addDepartment(@PathVariable (value = "companyId") UUID companyId, @RequestBody Department department) {
+    public Department addDepartment(@PathVariable (value = "companyId") UUID companyId,
+                                    @RequestBody Department department) {
         return companyService.getCompanyById(companyId).map(company -> {
             department.setCompany(company);
             return departmentService.addDepartment(department);
@@ -43,5 +45,19 @@ public class DepartmentController {
     @GetMapping("/departments")
     public List<Department> getDepartments() {
         return departmentService.getAllDepartments();
+    }
+
+    @PutMapping("/companies/{companyId}/departments/{id}")
+    public Department updateDepartment(@PathVariable UUID companyId,
+                                       @PathVariable UUID departmentId,
+                                       @RequestBody Department department) {
+        throw new NotYetImplementedException();
+    }
+
+    @DeleteMapping("/companies/{companyId}/departments/{id}")
+    public Department deleteDepartment(@PathVariable UUID companyId,
+                                       @PathVariable UUID departmentId,
+                                       @RequestBody Department department) {
+        throw new NotYetImplementedException();
     }
 }
