75cff086ad0a5aa07df5caa06ef220748855cb65
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-15 01:45:17 +0800

    src: main: java: csd: roster: service: WorkLocationServiceImpl: Add @Service annotation
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/WorkLocationServiceImpl.java
+++ src/main/java/csd/roster/service/WorkLocationServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class WorkLocationServiceImpl implements WorkLocationService {
     final private WorkLocationRepository workLocationRepository;
     final private DepartmentService departmentService;
