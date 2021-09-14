a5b10bd4a918def5c082a6b610cbf9004bc7ca74
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 15:32:45 +0800

    src: main: java: csd: roster: service: DepartmentService: Add methods for the interface
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/DepartmentService.java
+++ src/main/java/csd/roster/service/DepartmentService.java
@@ -1,4 +1,18 @@
 package csd.roster.service;
 
+import csd.roster.model.Department;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface DepartmentService {
+    Department addDepartment(Department department);
+
+    List<Department> getAllDepartments();
+
+    Department getDepartmentById(UUID id);
+
+    Department updateCompanyById(UUID id);
+
+    Department deleteDepartmentById(UUID id);
 }
