71425a57c14df4cf9102081d6fc704096ff96b4f
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 15:39:51 +0800

    src: main: java: csd: roster: service: DepartmentServiceImpl: Override interface methods and throw NotYetImplementedException
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/DepartmentServiceImpl.java
+++ src/main/java/csd/roster/service/DepartmentServiceImpl.java
@@ -1,4 +1,44 @@
 package csd.roster.service;
 
-public class DepartmentServiceImpl {
+import csd.roster.model.Department;
+import csd.roster.repository.DepartmentRepository;
+import org.hibernate.cfg.NotYetImplementedException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    @Override
+    public Department addDepartment(Department department) {
+        throw new NotYetImplementedException();
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        throw new NotYetImplementedException();
+    }
+
+    @Override
+    public Department getDepartmentById(UUID id) {
+        throw new NotYetImplementedException();
+    }
+
+    @Override
+    public Department updateCompanyById(UUID id) {
+        throw new NotYetImplementedException();
+    }
+
+    @Override
+    public Department deleteDepartmentById(UUID id) {
+        throw new NotYetImplementedException();
+    }
 }
