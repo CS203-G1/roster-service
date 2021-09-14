01e8b65597966d63a5d6ac976d1c0f9cc1aeca26
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-14 17:01:00 +0800

    src: main: java: csd: roster: exception: DepartmentNotFoundException: Create a runtime expection for when department is not found in database
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

---
+++ src/main/java/csd/roster/exception/DepartmentNotFoundException.java
@@ -0,0 +1,16 @@
+package csd.roster.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.UUID;
+
+@ResponseStatus(HttpStatus.NOT_FOUND) // 404 Error
+public class DepartmentNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DepartmentNotFoundException(UUID departmentId, UUID companyId) {
+        super(String.format("Could not find department %s from company %s", departmentId, companyId));
+    }
+
+}
