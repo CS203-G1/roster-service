9329a4d84718d61c66e98b9a0851200a2d39b393
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-15 10:33:05 +0800

    src: main: java: csd: roster: repository: WorkLocationRepository: Add findByIdAndDepartmentId method
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/WorkLocationRepository.java
+++ src/main/java/csd/roster/repository/WorkLocationRepository.java
@@ -4,8 +4,10 @@ import csd.roster.model.WorkLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WorkLocationRepository extends JpaRepository<WorkLocation, UUID> {
+    Optional<WorkLocation> findByIdAndDepartmentId(UUID workLocationId, UUID departmentId);
 }
