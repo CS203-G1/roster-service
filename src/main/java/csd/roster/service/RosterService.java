792d86052879aebd69cf8baa103aebf7ff80e31d
Jerald <jeraldssw@gmail.com>
2021-09-15 23:48:29 +0800

    Update RosterService interface method arguments

--- src/main/java/csd/roster/service/RosterService.java
+++ src/main/java/csd/roster/service/RosterService.java
@@ -1,15 +1,15 @@
 package csd.roster.service;
 
-import csd.roster.model.Roster;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+import csd.roster.model.Roster;
+
 public interface RosterService {
-    Roster getRosterById(UUID rosterId);
-    Roster getRosterByIdAndWorkLocationId(UUID rosterId, UUID workLocationId);
-    List<Roster> getRostersByWorkLocationId(UUID workLocationId);
-    void delete(UUID rosterId, UUID workLocationId);
-    Roster update(UUID rosterId, UUID workLocationId, LocalDate date);
+    Roster addRoster(UUID companyId, UUID departmentId, UUID workLocationId, Roster roster);
+    List<Roster> getRosters(UUID companyId, UUID departmentId, UUID workLocationId);
+    Roster getRoster(UUID companyId, UUID departmentId, UUID workLocationId, UUID rosterId);
+    void delete(UUID companyId, UUID departmentId, UUID workLocationId, UUID rosterId);
+    Roster update(UUID companyId, UUID departmentId, UUID workLocationId, UUID rosterId, LocalDate date);
 }
