0e64f1af98066c831a74e4c5506cd5492edbc9ae
Jerald <jeraldssw@gmail.com>
2021-09-15 21:52:05 +0800

    Fix query keywords - replace companyId with workLocationId

--- src/main/java/csd/roster/repository/RosterRepository.java
+++ src/main/java/csd/roster/repository/RosterRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface RosterRepository extends JpaRepository<Roster, UUID> {
-    Optional<Roster> findByIdAndCompanyId(UUID rosterId, UUID companyId);
+    Optional<Roster> findByIdAndWorkLocationId(UUID rosterId, UUID workLocationId);
 }
