e1e19ab394bf5c4b61d7fd690b299cbb2db5b4ce
Jerald <jeraldssw@gmail.com>
2021-09-15 22:42:48 +0800

    Create findByWorkLocationId method

--- src/main/java/csd/roster/repository/RosterRepository.java
+++ src/main/java/csd/roster/repository/RosterRepository.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import csd.roster.model.Roster;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RosterRepository extends JpaRepository<Roster, UUID> {
     Optional<Roster> findByIdAndWorkLocationId(UUID rosterId, UUID workLocationId);
+    List<Roster> findByWorkLocationId(UUID workLocationId);
 }
