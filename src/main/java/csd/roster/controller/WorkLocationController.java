fb0e439a9d173dafd2d15d9b1211cde7e94db08a
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-15 10:06:11 +0800

    src: main: java: csd: roster: controller: WorkLocationController: Add addDepartment API in WorkLocationController
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/WorkLocationController.java
+++ src/main/java/csd/roster/controller/WorkLocationController.java
@@ -2,6 +2,7 @@ package csd.roster.controller;
 
 import csd.roster.exception.ResourceNotFoundException;
 import csd.roster.model.Department;
+import csd.roster.model.WorkLocation;
 import csd.roster.service.WorkLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,8 +16,21 @@ import java.util.UUID;
 public class WorkLocationController {
     final private WorkLocationService workLocationService;
 
+    @Autowired
     public WorkLocationController(WorkLocationService workLocationService) {
         this.workLocationService = workLocationService;
+    }
+
+    @PostMapping("/companies/{companyId}/departments/{departmentId}/work-locations")
+    public ResponseEntity<?> addDepartment(@PathVariable (value = "companyId") UUID companyId,
+                                           @PathVariable (value = "departmentId") UUID departmentId,
+                                           @RequestBody WorkLocation workLocation) {
+        try {
+            WorkLocation newWorkLocation = workLocationService.add(companyId, departmentId, workLocation);
+            return new ResponseEntity<>(newWorkLocation, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
 }
