ac644db8664d571778fb02ffa72da0242fa175d7
Jerald <jeraldssw@gmail.com>
2021-09-17 19:41:43 +0800

    Remove wildcard imports

--- src/main/java/csd/roster/model/Roster.java
+++ src/main/java/csd/roster/model/Roster.java
@@ -1,10 +1,22 @@
 package csd.roster.model;
 
-import lombok.*;
-import javax.persistence.*;
-
 import java.time.LocalDate;
 import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
