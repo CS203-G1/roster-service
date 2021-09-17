7975048a9156786f82cab6493e3bfdd3d90b1f50
Jerald <jeraldssw@gmail.com>
2021-09-17 20:57:53 +0800

    Add getById method for WorkLocation

--- src/main/java/csd/roster/service/WorkLocationService.java
+++ src/main/java/csd/roster/service/WorkLocationService.java
@@ -1,21 +1,15 @@
 package csd.roster.service;
 
-import csd.roster.model.Department;
-import csd.roster.model.WorkLocation;
-import org.hibernate.jdbc.Work;
-
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
+
+import csd.roster.model.WorkLocation;
 
 public interface WorkLocationService {
     WorkLocation add(UUID companyId,UUID departmentId, WorkLocation workLocation);
-
     List<WorkLocation> getWorkLocationsFromDepartment(UUID departmentId);
-
     void delete(UUID companyId, UUID departmentId, UUID workLocationId);
-
     WorkLocation update(UUID companyId, UUID departmentId, UUID workLocationId, WorkLocation workLocation);
-
     WorkLocation get(UUID companyId, UUID departmentId, UUID workLocationId);
+    WorkLocation getById(UUID workLocationId);
 }
