4eeda051d569b91427c7327f418d623373d3528d
Jerald <jeraldssw@gmail.com>
2021-09-17 20:54:05 +0800

    Remove wildcard imports in WorkLocation model

--- src/main/java/csd/roster/model/WorkLocation.java
+++ src/main/java/csd/roster/model/WorkLocation.java
@@ -1,14 +1,26 @@
 package csd.roster.model;
 
-import lombok.*;
-import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import java.util.UUID;
-import java.util.List;
-
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
