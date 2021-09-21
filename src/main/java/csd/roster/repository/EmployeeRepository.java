6f11406805e55c0d65550dff533173d64610c2b5
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 03:02:04 +0800

    Extend JPARepository for JPA functionalities
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/EmployeeRepository.java
+++ src/main/java/csd/roster/repository/EmployeeRepository.java
@@ -1,4 +1,11 @@
 package csd.roster.repository;
 
-public interface EmployeeRepository {
+import csd.roster.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
 }
