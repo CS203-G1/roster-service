cc9973d38036607a36752cba9074f49bdfe424c1
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 03:09:05 +0800

    Temporarily implement EmployeeService
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/EmployeeServiceImpl.java
+++ src/main/java/csd/roster/service/EmployeeServiceImpl.java
@@ -1,4 +1,32 @@
 package csd.roster.service;
 
-public class EmployeeServiceImpl {
+import java.util.List;
+import java.util.UUID;
+
+import csd.roster.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public Employee getEmployee(UUID employeeId) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(UUID employeeId) {
+
+    }
+
+    @Override
+    public Employee updateEmployee(UUID employeeId, Employee employee) {
+        return null;
+    }
 }
