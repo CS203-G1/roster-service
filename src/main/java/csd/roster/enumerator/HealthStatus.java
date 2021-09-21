f9f16df5450148ac8d4636302286b34b3163cbe0
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 02:55:26 +0800

    Define Enumerator HealthStatus and add TODO to check naming with team
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/enumerator/HealthStatus.java
+++ src/main/java/csd/roster/enumerator/HealthStatus.java
@@ -1,2 +1,6 @@
-package csd.roster.enumerator;public enum HealthStatus {
+package csd.roster.enumerator;
+
+public enum HealthStatus {
+    // TODO: Check with team if they are okay with this naming
+    HEALTHY, ILL, COVID;
 }
