d3c38532d2f08b0c67c0027252549b224526326a
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 03:05:22 +0800

    List down the basic CRUD operators needed for Employee class
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/EmployeeService.java
+++ src/main/java/csd/roster/service/EmployeeService.java
@@ -1,4 +1,13 @@
 package csd.roster.service;
 
+import csd.roster.model.Employee;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface EmployeeService {
+    Employee addEmployee(Employee employee);
+    Employee getEmployee(UUID employeeId);
+    void deleteEmployee(UUID employeeId);
+    Employee updateEmployee(UUID employeeId, Employee employee);
 }
