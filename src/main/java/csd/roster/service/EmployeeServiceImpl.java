2629eeb2be8af8116f52e7ae604df2ac5fd51303
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 03:10:05 +0800

    Inject dependency EmployeeRepository
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/EmployeeServiceImpl.java
+++ src/main/java/csd/roster/service/EmployeeServiceImpl.java
@@ -4,11 +4,18 @@ import java.util.List;
 import java.util.UUID;
 
 import csd.roster.model.Employee;
+import csd.roster.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @Override
     public Employee addEmployee(Employee employee) {
