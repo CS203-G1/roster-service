dd6359a04802b99073698dfbced99eb335ddc725
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 02:52:32 +0800

    Define Enumerator VaccineBrand and add explanations to what each are specifically
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/enumerator/VaccineBrand.java
+++ src/main/java/csd/roster/enumerator/VaccineBrand.java
@@ -1,2 +1,10 @@
-package csd.roster.enumerator;public enum VaccineBrand {
+package csd.roster.enumerator;
+
+public enum VaccineBrand {
+    // Currently we are just keeping track of the vaccines that are deployed in Singapore
+    // Refer to : https://en.wikipedia.org/wiki/COVID-19_vaccination_in_Singapore#Vaccines_used
+
+    // Note that Pfizer-BioNTech/Cominarty is represented by PFIZER
+    // BBIBP-CorV is represented by BBIBP
+    PFIZER, MODERNA, CORONA_VAC, BBIBP;
 }
