2b6c94a11686897fb9fca722208ac3a341352b5e
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 02:56:43 +0800

    Add Employee class and add import statements required
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/model/Employee.java
+++ src/main/java/csd/roster/model/Employee.java
@@ -1,4 +1,29 @@
+
 package csd.roster.model;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import csd.roster.enumerator.HealthStatus;
+import csd.roster.enumerator.VaccinationStatus;
+import csd.roster.enumerator.VaccineBrand;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Employee {
+    
 }
