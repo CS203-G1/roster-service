e79156dbcc0cb4e7f40ea46715db09b690d963b5
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 18:34:02 +0800

    Remove WorkLocationService dependency
    Add DepartmentService dependency
    
    Add CompanyId to method params
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/EmployeeServiceImpl.java
+++ src/main/java/csd/roster/service/EmployeeServiceImpl.java
@@ -3,8 +3,8 @@ package csd.roster.service;
 import java.util.List;
 import java.util.UUID;
 
+import csd.roster.model.Department;
 import csd.roster.model.Employee;
-import csd.roster.model.WorkLocation;
 import csd.roster.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,35 +12,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
-    private WorkLocationService workLocationService;
+    private DepartmentService departmentService;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository, WorkLocationService workLocationService) {
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, DepartmentService departmentService) {
         this.employeeRepository = employeeRepository;
-        this.workLocationService = workLocationService;
+        this.departmentService = departmentService;
     }
 
     @Override
-    public Employee addEmployee(UUID workLocationId, Employee employee) {
+    public Employee addEmployee(UUID companyId, UUID departmentId, Employee employee) {
         // TODO: wait for workLocation updates to be merged to develop branch
-//        WorkLocation workLocation = workLocationService.getWorkLocationById(workLocationId);
-//        roster.setWorkLocation(workLocation);
+        Department department = departmentService.getDepartmentByIdAndCompanyId(departmentId, companyId);
+        employee.setDepartment(department);
 
         return employeeRepository.save(employee);
     }
 
     @Override
-    public Employee getEmployee(UUID workLocationId, UUID employeeId) {
+    public Employee getEmployee(UUID companyId, UUID departmentId, UUID employeeId) {
         return null;
     }
 
     @Override
-    public void deleteEmployee(UUID workLocationId, UUID employeeId) {
-
+    public void deleteEmployee(UUID companyId, UUID departmentId, UUID employeeId) {
+        return null
     }
 
     @Override
-    public Employee updateEmployee(UUID workLocationId, UUID employeeId, Employee employee) {
+    public Employee updateEmployee(UUID departmentId, UUID employeeId, Employee employee) {
         return null;
     }
 }
