f6387b1f3a59bdeb5204f76a165376e8b9e3f7a5
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 20:36:10 +0800

    Inject dependency EmployeeService with Autowired
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/EmployeeController.java
+++ src/main/java/csd/roster/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 
+import csd.roster.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,4 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
 }
