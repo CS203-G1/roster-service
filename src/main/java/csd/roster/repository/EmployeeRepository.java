ac6db9bc1be585bfb8dbf385fed866bfc3fe8ed3
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-22 21:58:46 +0800

    Add findByIdAndDepartmentId method
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/EmployeeRepository.java
+++ src/main/java/csd/roster/repository/EmployeeRepository.java
@@ -1,11 +1,14 @@
 package csd.roster.repository;
 
 import csd.roster.model.Employee;
+import csd.roster.model.WorkLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+    Optional<WorkLocation> findByIdAndDepartmentId(UUID employeeId, UUID departmentId);
 }
