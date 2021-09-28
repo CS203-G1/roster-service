755fb655fcda5cc037bc6ecff1df659253c46973
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 02:16:03 +0800

    Add relevant imports for a validator to work
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/validator/DateTimesValidator.java
+++ src/main/java/csd/roster/validator/DateTimesValidator.java
@@ -1,2 +1,14 @@
-package csd.roster.validator;public class DateTimesValidator {
+package csd.roster.validator;
+
+import csd.roster.annotation.ValidDateTimes;
+import csd.roster.model.RosterEmployee;
+import org.springframework.beans.BeanWrapperImpl;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDateTime;
+
+// Custom Validator to validate whether fromDateTime is before toDateTime
+// reference: https://www.baeldung.com/spring-mvc-custom-validator
+public class DateTimesValidator implements ConstraintValidator<ValidDateTimes, RosterEmployee> {
 }
