c448a250df20bf4dba2d1fb0fe1e6da42403b44c
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 01:01:15 +0800

    Add relevant import statements and extend JPARepository
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/RosterEmployeeRepository.java
+++ src/main/java/csd/roster/repository/RosterEmployeeRepository.java
@@ -1,4 +1,11 @@
 package csd.roster.repository;
 
-public interface RosterEmployeeRepository {
+import csd.roster.model.RosterEmployee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RosterEmployeeRepository extends JpaRepository<RosterEmployee, UUID> {
 }
