80a1cbe954bb50df228c0ac3aff3d62129bf07ca
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 00:38:00 +0800

    Initialize RosterEmployee as an Entity class.
    Add lombok import statements and add annotations to the class.
    
    Add ManyToOne relationship to roster
    
    Add ManyToOne relationship to employee
    
    Add attributes from_datetime, to_datetime and current_health_status
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/model/RosterEmployee.java
+++ src/main/java/csd/roster/model/RosterEmployee.java
@@ -1,4 +1,50 @@
 package csd.roster.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.*;
+
+import csd.roster.enumerator.HealthStatus;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class RosterEmployee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    // Note that in lecture notes prof used many-to-many since there is no need to use relationship entity
+    // Here I am using Many-To-One and One-To-Many since RosterEmployee is a relationship entity
+    // Reference: https://www.baeldung.com/jpa-many-to-many
+
+    @ManyToOne
+    @JoinColumn(name = "roster_id")
+    Roster roster;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    Employee employee;
+
+    @Column(name = "from_datetime")
+    private LocalDateTime from_datetime;
+
+    @Column(name = "to_datetime")
+    private LocalDateTime to_datetime;
+
+    @Column(name = "current_health_status")
+    private HealthStatus current_health_status;
 }
