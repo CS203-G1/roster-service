544d3a3989df94e749c0a8b637ff5e20c9dab2f3
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 02:14:28 +0800

    Add @annotations copied from https://www.baeldung.com/spring-mvc-custom-validator for validation annotation to work
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/annotation/ValidDateTimes.java
+++ src/main/java/csd/roster/annotation/ValidDateTimes.java
@@ -1,2 +1,17 @@
-package csd.roster.annotation;public @interface ValidDateTimes {
+package csd.roster.annotation;
+
+import csd.roster.validator.DateTimesValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// For annotations, refer to: https://stackoverflow.com/a/43787036/16331209
+// For baeldung reference: https://www.baeldung.com/spring-mvc-custom-validator
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = DateTimesValidator.class)
+public @interface ValidDateTimes {
 }
