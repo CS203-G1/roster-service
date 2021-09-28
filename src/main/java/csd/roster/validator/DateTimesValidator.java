db731e57bc8059cd2a2953d865ddaf859eaedaa2
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 02:16:31 +0800

    Add private fields to store field names as instance attributes
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/validator/DateTimesValidator.java
+++ src/main/java/csd/roster/validator/DateTimesValidator.java
@@ -11,4 +11,8 @@ import java.time.LocalDateTime;
 // Custom Validator to validate whether fromDateTime is before toDateTime
 // reference: https://www.baeldung.com/spring-mvc-custom-validator
 public class DateTimesValidator implements ConstraintValidator<ValidDateTimes, RosterEmployee> {
+
+    private String fromDateTimeFieldName;
+    private String toDateTimeFieldName;
+
 }
