dece1725bbd7a3ecd8e2e26e1cede7c84eb902da
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 17:10:15 +0800

    Implement RuntimeException
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/exception/RuleViolatedException.java
+++ src/main/java/csd/roster/exception/RuleViolatedException.java
@@ -1,2 +1,14 @@
-package csd.roster.exception;public class RuleViolatedException {
+package csd.roster.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND) // 404 Error
+public class RuleViolatedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public RuleViolatedException(String reason) {
+        super(reason);
+    }
 }
