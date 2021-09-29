3b3213afeb955c175e997eda4ace293ec8b80772
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 17:13:48 +0800

    Implement RuleViolatedException
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/exception/EmployeeNotHealthyException.java
+++ src/main/java/csd/roster/exception/EmployeeNotHealthyException.java
@@ -1,4 +1,10 @@
 package csd.roster.exception;
 
-public class EmployeeNotHealthyException {
+import csd.roster.model.Employee;
+import org.dom4j.rule.Rule;
+
+public class EmployeeNotHealthyException extends RuleViolatedException {
+    public EmployeeNotHealthyException(Employee employee) {
+        super(String.format("Employee %s is not healthy", employee.getId()));
+    }
 }
