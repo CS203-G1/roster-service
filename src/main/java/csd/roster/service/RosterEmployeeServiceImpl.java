fb5def979fe10c04da79df821a30ea7132dc653e
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 10:43:22 +0800

    Add Service and Autowired annotation
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/RosterEmployeeServiceImpl.java
+++ src/main/java/csd/roster/service/RosterEmployeeServiceImpl.java
@@ -4,14 +4,18 @@ import csd.roster.model.Employee;
 import csd.roster.model.Roster;
 import csd.roster.model.RosterEmployee;
 import csd.roster.repository.RosterEmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class RosterEmployeeServiceImpl implements RosterEmployeeService {
     private final RosterEmployeeRepository rosterEmployeeRepository;
     private final RosterService rosterService;
     private final EmployeeService employeeService;
 
+    @Autowired
     public RosterEmployeeServiceImpl(RosterEmployeeRepository rosterEmployeeRepository,
                                      RosterService rosterService,
                                      EmployeeService employeeService) {
