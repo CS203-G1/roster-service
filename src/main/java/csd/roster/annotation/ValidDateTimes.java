754301b3532d7d9668bf0a2c28928a4a16d359cd
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 12:19:28 +0800

    Add payload and group to annotation
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/annotation/ValidDateTimes.java
+++ src/main/java/csd/roster/annotation/ValidDateTimes.java
@@ -3,6 +3,7 @@ package csd.roster.annotation;
 import csd.roster.validator.DateTimesValidator;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -19,4 +20,6 @@ public @interface ValidDateTimes {
 
     // Custom message
     String message() default "FromDateTime must be before ToDateTime";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
