cad8a59a58974bd2c02a7b17ea042ff06e8436ef
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 10:14:30 +0800

    Add service method addRosterEmployee in RosterEmployeeService as a method to create a RosterEmployee object
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/RosterEmployeeService.java
+++ src/main/java/csd/roster/service/RosterEmployeeService.java
@@ -1,4 +1,10 @@
 package csd.roster.service;
 
+import csd.roster.model.RosterEmployee;
+
+import java.util.UUID;
+
 public interface RosterEmployeeService {
+    RosterEmployee addRosterEmployee(UUID rosterId, UUID employeeId, RosterEmployee rosterEmployee);
+
 }
