717a8077936066aed61f3d8db4824bdbcf4cad42
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 10:43:43 +0800

    Add dependency for RosterEmployeeController
    Use constructor injection to inject RosterEmployeeService
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/RosterEmployeeController.java
+++ src/main/java/csd/roster/controller/RosterEmployeeController.java
@@ -1,4 +1,29 @@
 package csd.roster.controller;
 
+import java.util.List;
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import csd.roster.service.RosterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import csd.roster.model.RosterEmployee;
+import csd.roster.service.RosterEmployeeService;
+
+@RestController
 public class RosterEmployeeController {
+    private RosterEmployeeService rosterEmployeeService;
+
+    @Autowired
+    public RosterEmployeeController(RosterEmployeeService rosterEmployeeService) {
+        this.rosterEmployeeService = rosterEmployeeService;
+    }
 }
