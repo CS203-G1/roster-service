b6ed31ee3175c0dd1e6b80273280963d7612c95f
Bing Yu <bingyu.yap.21@gmail.com>
2021-09-29 10:20:23 +0800

    Add dependencies rosterEmployeeRepository, rosterService and employeeService
    Add constructor injection for abovementioned dependencies
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/RosterEmployeeServiceImpl.java
+++ src/main/java/csd/roster/service/RosterEmployeeServiceImpl.java
@@ -1,12 +1,27 @@
 package csd.roster.service;
 
+import csd.roster.model.Roster;
 import csd.roster.model.RosterEmployee;
+import csd.roster.repository.RosterEmployeeRepository;
 
 import java.util.UUID;
 
 public class RosterEmployeeServiceImpl implements RosterEmployeeService {
+    private final RosterEmployeeRepository rosterEmployeeRepository;
+    private final RosterService rosterService;
+    private final EmployeeService employeeService;
+
+    public RosterEmployeeServiceImpl(RosterEmployeeRepository rosterEmployeeRepository,
+                                     RosterService rosterService,
+                                     EmployeeService employeeService) {
+        this.rosterEmployeeRepository = rosterEmployeeRepository;
+        this.rosterService = rosterService;
+        this.employeeService = employeeService;
+    }
+
     @Override
     public RosterEmployee addRosterEmployee(UUID rosterId, UUID employeeId, RosterEmployee rosterEmployee) {
+
         return null;
     }
 }
