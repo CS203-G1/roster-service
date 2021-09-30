2a44f576fae45fd0cc99ece42067120a029bf561
Gary Peabody <aikarvin99@gmail.com>
2021-09-30 16:17:02 +0800

    Add test for getCompanyById in test/java/csd.roster/service/CompanyServiceTest
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/test/java/csd/roster/service/CompanyServiceTest.java
+++ src/test/java/csd/roster/service/CompanyServiceTest.java
@@ -11,10 +11,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CompanyServiceTest {
@@ -26,20 +26,32 @@ public class CompanyServiceTest {
     private CompanyServiceImpl companyService;
 
     @Test
-    void addCompany_newName_ReturnSavedCompany(){
-        // Arrange
+    void addCompany_ReturnSavedCompany(){
         Company company = new Company(UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0"), "Eppal", null);
 
         when(companies.save(any(Company.class))).thenReturn(company);
 
-        // Act
         Company savedCompany = companyService.addCompany(company);
 
-        // Assert
         assertNotNull(savedCompany);
 
+        verify(companies, times(1)).save(company);
 
+    }
 
+    @Test
+    void getCompanyByID(){
+        UUID id = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
+        Company company = new Company(UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0"), "Eppal", null);
+
+        when(companies.findById(any(UUID.class))).thenReturn(java.util.Optional.of(company));
+
+        Company foundCompany = companyService.getCompanyById(id);
+
+        assertNotNull(foundCompany);
+        assertEquals(company, foundCompany);
+
+        verify(companies, times(1)).findById(id);
 
     }
 }
