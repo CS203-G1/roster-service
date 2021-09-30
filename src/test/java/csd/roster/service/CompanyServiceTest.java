c6dc5244810d60351d8a26df53288f9c2d1c9b66
Gary Peabody <aikarvin99@gmail.com>
2021-09-30 18:52:13 +0800

    Add test for CompanyService#getAllCompanies()
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/test/java/csd/roster/service/CompanyServiceTest.java
+++ src/test/java/csd/roster/service/CompanyServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,7 +27,7 @@ public class CompanyServiceTest {
     private CompanyServiceImpl companyService;
 
     @Test
-    void addCompany_ReturnSavedCompany(){
+    void addCompany_NewCompany_ReturnSavedCompany(){
         Company company = new Company(UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0"), "Eppal", null);
 
         when(companies.save(any(Company.class))).thenReturn(company);
@@ -40,9 +41,9 @@ public class CompanyServiceTest {
     }
 
     @Test
-    void getCompanyByID(){
+    void getCompanyById_CompanyExists_ReturnCompany(){
         UUID id = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
-        Company company = new Company(UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0"), "Eppal", null);
+        Company company = new Company(id, "Eppal", null);
 
         when(companies.findById(any(UUID.class))).thenReturn(java.util.Optional.of(company));
 
@@ -54,4 +55,49 @@ public class CompanyServiceTest {
         verify(companies, times(1)).findById(id);
 
     }
+
+    @Test
+    void getAllCompanies_ThreeCompaniesExist_ReturnListOfCompanies(){
+        UUID id1 = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
+        Company company1 = new Company(id1, "Eppal", null);
+
+        UUID id2 = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
+        Company company2 = new Company(id1, "PayPal", null);
+
+        UUID id3 = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
+        Company company3 = new Company(id1, "PeePal", null);
+
+        when(companies.findAll()).thenReturn(new ArrayList<Company>());
+        when(companies.save(any(Company.class)))
+                .thenReturn(company1)
+                .thenReturn(company2)
+                .thenReturn(company3);
+
+        List<Company> allCompanies = companyService.getAllCompanies();
+        Company savedCompany1 = companyService.addCompany(company1);
+        Company savedCompany2 = companyService.addCompany(company2);
+        Company savedCompany3 = companyService.addCompany(company3);
+
+        allCompanies.add(savedCompany1);
+        allCompanies.add(savedCompany2);
+        allCompanies.add(savedCompany3);
+
+        assertEquals(company1,savedCompany1);
+        assertEquals(company2,savedCompany2);
+        assertEquals(company3,savedCompany3);
+
+        assertNotNull(allCompanies);
+        assertEquals(3,allCompanies.size());
+
+        verify(companies, times(3)).save(any(Company.class));
+        verify(companies, times(1)).findAll();
+
+    }
+//    @Test
+//    void updateCompanyById_CompanyExists_ReturnUpdatedCompany(){
+//        UUID id = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
+//        Company company = new Company(id, "Eppal", null);
+//
+//
+//    }
 }
