186641dbf7e9efce956e3c4a8fa6ec55a1e768e5
Gary Peabody <aikarvin99@gmail.com>
2021-10-01 16:58:20 +0800

    Add test method for DepartmentService#getDepartmentById
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/test/java/csd/roster/service/DepartmentServiceTest.java
+++ src/test/java/csd/roster/service/DepartmentServiceTest.java
@@ -52,38 +52,29 @@ public class DepartmentServiceTest {
 
     @Test
     public void getAllDepartments_AfterAddingThreeDepartments_ReturnListOfDepartments(){
-        UUID company_id = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
-        Company company = new Company(company_id, "Eppal", null);
-
-        UUID dept1_id = UUID.fromString("decf52aa-94a8-48c5-abca-04f037d98e56");
-        Department department1 = new Department(dept1_id, null, "Marketing");
-
-        UUID dept2_id = UUID.fromString("0fb6f2a1-597b-42da-9c69-4b2d766dce46");
-        Department department2 = new Department(dept2_id, null, "Software Engineering");
-
-        UUID dept3_id = UUID.fromString("0d7a075f-a35e-4b5c-b960-6f4b47df1ce7");
-        Department department3 = new Department(dept3_id, null, "Human Resources");
-
-        when(companyService.getCompanyById(any(UUID.class))).thenReturn(company);
         when(departments.findAll()).thenReturn(new ArrayList<Department>());
-        when(departments.save(any(Department.class)))
-                .thenReturn(department1)
-                .thenReturn(department2)
-                .thenReturn(department3);
 
-        Department savedDepartment1 = departmentService.add(company_id, department1);
-        Department savedDepartment2 = departmentService.add(company_id, department2);
-        Department savedDepartment3 = departmentService.add(company_id, department3);
         List<Department> allDepartments = departmentService.getAllDepartments();
 
-        assertEquals(department1, savedDepartment1);
-        assertEquals(department2, savedDepartment2);
-        assertEquals(department3, savedDepartment3);
         assertNotNull(allDepartments);
-
-        verify(departments, times(3)).save(any(Department.class));
         verify(departments, times(1)).findAll();
 
 
+    }
+
+    @Test
+    public void getDepartmentById_DepartmentExists(){
+        UUID company_id = UUID.fromString("a4ccc2c4-0426-41a2-b904-f7a941ba27e0");
+        Company company = new Company(company_id, "Eppal", null);
+
+        UUID dept_id = UUID.fromString("decf52aa-94a8-48c5-abca-04f037d98e56");
+        Department department = new Department(dept_id, company, "Marketing");
+
+        when(departments.findById(any(UUID.class))).thenReturn(java.util.Optional.of(department));
+
+        Department foundDepartment = departmentService.getDepartmentById(dept_id);
+
+        assertEquals(department,foundDepartment);
+        verify(departments, times(1)).findById(dept_id);
     }
 }
