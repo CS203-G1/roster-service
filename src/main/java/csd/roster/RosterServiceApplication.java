a8509a22e3dc7230ec23dc50478d72d7d56c0fab
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-01 11:52:09 +0800

    Add @Bean to update corsFilter
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/RosterServiceApplication.java
+++ src/main/java/csd/roster/RosterServiceApplication.java
@@ -2,11 +2,33 @@ package csd.roster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+@EnableResourceServer
 @SpringBootApplication
 public class RosterServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RosterServiceApplication.class, args);
+	}
+
+	@Bean // Note
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
