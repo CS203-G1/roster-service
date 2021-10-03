5ac7ac973be868c58429dcfafe5450fb57210219
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 04:05:25 +0800

    Add custom query to get employees by company id
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/EmployeeRepository.java
+++ src/main/java/csd/roster/repository/EmployeeRepository.java
@@ -3,8 +3,11 @@ package csd.roster.repository;
 import csd.roster.model.Employee;
 import csd.roster.model.WorkLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,5 +16,6 @@ import java.util.UUID;
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
     Optional<Employee> findByIdAndDepartmentId(UUID employeeId, UUID departmentId);
 
-    Iterable<Employee> findAllByCompanyId(UUID companyId);
+    @Query("select e from Employee e where e.department.company.id = :id")
+    Collection<Employee> findAllByCompanyId(@Param("id") UUID companyId);
 }
