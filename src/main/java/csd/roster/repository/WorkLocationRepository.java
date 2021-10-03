4cc8b39eab7665951a5547521354bbe9f66cc22a
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 04:15:52 +0800

    Add custom query to query remote work location by company id
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/WorkLocationRepository.java
+++ src/main/java/csd/roster/repository/WorkLocationRepository.java
@@ -2,6 +2,8 @@ package csd.roster.repository;
 
 import csd.roster.model.WorkLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ import java.util.UUID;
 @Repository
 public interface WorkLocationRepository extends JpaRepository<WorkLocation, UUID> {
     Optional<WorkLocation> findByIdAndDepartmentId(UUID workLocationId, UUID departmentId);
+
+    @Query("select wl from WorkLocation wl where wl.name = 'remote' and wl.id = :id")
+    Optional<WorkLocation> findRemoteWorkLocationByCompanyId(@Param("id") UUID companyId);
 }
