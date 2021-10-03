8fa9f0e5e57ce268ee3d091a052c3e043c499b4e
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-03 20:26:56 +0800

    Add attributes for the WorkingStatisticResponseModel
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/response_model/WorkingStatisticResponseModel.java
+++ src/main/java/csd/roster/response_model/WorkingStatisticResponseModel.java
@@ -1,2 +1,19 @@
-package csd.roster.response_model;public class WorkingStatisticResponseModel {
+package csd.roster.response_model;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class WorkingStatisticResponseModel {
+    private UUID companyId;
+
+    private int onsiteCount;
+
+    private int remoteCount;
 }
