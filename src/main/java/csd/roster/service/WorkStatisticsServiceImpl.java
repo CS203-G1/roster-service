af85844b39b684d31777fbb7aa50697dfa515968
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 04:17:21 +0800

    Get remote workLocation
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
+++ src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
@@ -1,5 +1,6 @@
 package csd.roster.service;
 
+import csd.roster.model.WorkLocation;
 import csd.roster.response_model.WorkingStatisticResponseModel;
 
 import java.util.UUID;
@@ -7,14 +8,20 @@ import java.util.UUID;
 public class WorkStatisticsServiceImpl implements WorkStatisticsService {
     private CompanyService companyService;
     private RosterEmployeeService rosterEmployeeService;
+    private WorkLocationService workLocationService;
 
-    public WorkStatisticsServiceImpl(CompanyService companyService, RosterEmployeeService rosterEmployeeService) {
+    public WorkStatisticsServiceImpl(CompanyService companyService,
+                                     RosterEmployeeService rosterEmployeeService,
+                                     WorkLocationService workLocationService) {
         this.companyService = companyService;
         this.rosterEmployeeService = rosterEmployeeService;
+        this.workLocationService = workLocationService;
     }
 
     @Override
     public WorkingStatisticResponseModel getCurrentWorkStatisticsByCompany(UUID companyId) {
+        WorkLocation remoteWorkLocation = workLocationService.getRemoteWorkLocationByCompanyId(companyId);
+
         return null;
     }
 }
