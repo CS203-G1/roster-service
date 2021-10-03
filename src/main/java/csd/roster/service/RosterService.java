ade2a2825ef2998b6ac243bdcb681ba043066998
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 04:41:42 +0800

    Add getCurrentRosterByWorkLocation method
    Format file
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/RosterService.java
+++ src/main/java/csd/roster/service/RosterService.java
@@ -4,12 +4,20 @@ import java.util.List;
 import java.util.UUID;
 
 import csd.roster.model.Roster;
+import csd.roster.model.WorkLocation;
 
 public interface RosterService {
     Roster addRoster(UUID workLocationId, Roster roster);
+
     List<Roster> getRosters(UUID workLocationId);
+
     Roster getRoster(UUID workLocationId, UUID rosterId);
+
     Roster getRoster(UUID rosterId);
+
     void deleteRoster(UUID workLocationId, UUID rosterId);
+
     Roster updateRoster(UUID workLocationId, UUID rosterId, Roster roster);
+
+    Roster getCurrentRosterByWorkLocation(WorkLocation workLocation);
 }
