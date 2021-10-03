e53cde6265d53e5322b322de55de085db53bca9d
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 04:42:40 +0800

    Add getCurrentRosterEmployeesByCompany method
    Add getCurrentRemoteRosterEmployeesByCompany method
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/WorkStatisticsService.java
+++ src/main/java/csd/roster/service/WorkStatisticsService.java
@@ -1,9 +1,13 @@
 package csd.roster.service;
 
+import csd.roster.model.RosterEmployee;
 import csd.roster.response_model.WorkingStatisticResponseModel;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface WorkStatisticsService {
     WorkingStatisticResponseModel getCurrentWorkStatisticsByCompany(UUID companyId);
+    Set<RosterEmployee> getCurrentRosterEmployeesByCompany(UUID companyId);
+    Set<RosterEmployee> getCurrentRemoteRosterEmployeesByCompany(UUID companyId);
 }
