640219304a095e9ae985e8a35fab4d5a1651b1e9
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 04:43:09 +0800

    Update dependencies
    Add getCurrentRosterEmployeesByCompany
    
    Implement  getCurrentRemoteRosterEmployeesByCompany method
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
+++ src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
@@ -1,27 +1,56 @@
 package csd.roster.service;
 
+import csd.roster.model.Company;
+import csd.roster.model.Roster;
+import csd.roster.model.RosterEmployee;
 import csd.roster.model.WorkLocation;
 import csd.roster.response_model.WorkingStatisticResponseModel;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public class WorkStatisticsServiceImpl implements WorkStatisticsService {
     private CompanyService companyService;
-    private RosterEmployeeService rosterEmployeeService;
     private WorkLocationService workLocationService;
+    private RosterService rosterService;
 
     public WorkStatisticsServiceImpl(CompanyService companyService,
-                                     RosterEmployeeService rosterEmployeeService,
-                                     WorkLocationService workLocationService) {
+                                     WorkLocationService workLocationService,
+                                     RosterService rosterService) {
         this.companyService = companyService;
-        this.rosterEmployeeService = rosterEmployeeService;
         this.workLocationService = workLocationService;
+        this.rosterService = rosterService;
     }
 
     @Override
     public WorkingStatisticResponseModel getCurrentWorkStatisticsByCompany(UUID companyId) {
-        WorkLocation remoteWorkLocation = workLocationService.getRemoteWorkLocationByCompanyId(companyId);
+        // Confirm that company exists
+        Company company = companyService.getCompanyById(companyId);
+
+        Set<RosterEmployee> remoteRosterEmployees = getCurrentRemoteRosterEmployeesByCompany(companyId);
+        int remoteEmployeesCount = remoteRosterEmployees.size();
+
+
 
         return null;
+    }
+
+    @Override
+    public Set<RosterEmployee> getCurrentRosterEmployeesByCompany(UUID companyId) {
+        return null;
+    }
+
+    @Override
+    public Set<RosterEmployee> getCurrentRemoteRosterEmployeesByCompany(UUID companyId) {
+
+        // Get the remote work location that belongs to this company
+        WorkLocation remoteWorkLocation = workLocationService.getRemoteWorkLocationByCompanyId(companyId);
+
+        // Get the roster for today for the remote work location
+        Roster remoteRoster = rosterService.getCurrentRosterByWorkLocation(remoteWorkLocation);
+
+        // Get the collection of roster employees that are assigned to this remote roster
+        return remoteRoster.getRosterEmployees();
     }
 }
