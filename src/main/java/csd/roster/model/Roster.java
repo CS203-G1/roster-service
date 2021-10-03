bc4f1e6701646c38f7f7c2130e08b0c3886244b4
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-03 23:19:08 +0800

    Add @JsonManagedReference on roster_employees attribute
    Add comment to rationalize usage of @JsonManagedReference
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/model/Roster.java
+++ src/main/java/csd/roster/model/Roster.java
@@ -13,8 +13,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -44,5 +47,9 @@ public class Roster {
     private WorkLocation workLocation;
 
     @OneToMany(mappedBy = "roster")
+    // @JsonManaged Reference means that this is the forward part of reference and will be serialized normally
+    // Done to prevent infinite recursion
+    // https://www.baeldung.com/jackson-bidirectional-relationships-and-infinite-recursion
+    @JsonManagedReference
     private Set<RosterEmployee> roster_employees;
 }
