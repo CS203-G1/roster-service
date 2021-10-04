4970f66a5f620fb9d189047152a80b48ee58d5e0
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 10:35:52 +0800

    Add custom JPA query findAllByCompanyId to get all WorkLocations of a company
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/WorkLocationRepository.java
+++ src/main/java/csd/roster/repository/WorkLocationRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,4 +16,7 @@ public interface WorkLocationRepository extends JpaRepository<WorkLocation, UUID
 
     @Query("select wl from WorkLocation wl where wl.name = 'remote' and wl.id = :id")
     Optional<WorkLocation> findRemoteWorkLocationByCompanyId(@Param("id") UUID companyId);
+
+    @Query("select wl from WorkLocation wl where wl.department.company.id = :id")
+    List<WorkLocation> findAllByCompanyId(@Param("id") UUID companyId);
 }
