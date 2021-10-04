cd247e9139d3a04da82a8127e780e23a39c5f57c
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 11:14:10 +0800

    Inject service dependencies
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/WorkStatisticsController.java
+++ src/main/java/csd/roster/controller/WorkStatisticsController.java
@@ -1,2 +1,14 @@
-package csd.roster.controller;public class WorkStatisticsController {
+package csd.roster.controller;
+
+import csd.roster.service.WorkStatisticsService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WorkStatisticsController {
+    final private WorkStatisticsService workStatisticsService;
+
+
+    public WorkStatisticsController(WorkStatisticsService workStatisticsService) {
+        this.workStatisticsService = workStatisticsService;
+    }
 }
