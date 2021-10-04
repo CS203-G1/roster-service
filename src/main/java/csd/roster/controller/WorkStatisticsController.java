114920db85cf4372e065e2227d14dd5946c36b96
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 12:28:46 +0800

    Add getCurrentWorkStatisticsByCompany method
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/WorkStatisticsController.java
+++ src/main/java/csd/roster/controller/WorkStatisticsController.java
@@ -1,7 +1,16 @@
 package csd.roster.controller;
 
+import csd.roster.exception.ResourceNotFoundException;
+import csd.roster.model.WorkLocation;
+import csd.roster.response_model.WorkingStatisticResponseModel;
 import csd.roster.service.WorkStatisticsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class WorkStatisticsController {
@@ -10,5 +19,10 @@ public class WorkStatisticsController {
 
     public WorkStatisticsController(WorkStatisticsService workStatisticsService) {
         this.workStatisticsService = workStatisticsService;
+    }
+
+    @GetMapping("/companies/{companyId}/work-statistics")
+    public WorkingStatisticResponseModel getCurrentWorkStatisticsByCompany(@PathVariable(value = "companyId") UUID companyId) {
+        return workStatisticsService.getCurrentWorkStatisticsByCompany(companyId);
     }
 }
