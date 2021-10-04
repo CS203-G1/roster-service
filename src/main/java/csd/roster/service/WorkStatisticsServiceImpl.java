1f793df740be2add6a203cee429ce872ccd1ba74
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-04 12:29:38 +0800

    Add stream to flatMap all current Roster Employees
    Add workingStatisticsModel
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
+++ src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
@@ -5,11 +5,14 @@ import csd.roster.model.Roster;
 import csd.roster.model.RosterEmployee;
 import csd.roster.model.WorkLocation;
 import csd.roster.response_model.WorkingStatisticResponseModel;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Service
 public class WorkStatisticsServiceImpl implements WorkStatisticsService {
     private CompanyService companyService;
     private WorkLocationService workLocationService;
@@ -26,14 +29,25 @@ public class WorkStatisticsServiceImpl implements WorkStatisticsService {
     @Override
     public WorkingStatisticResponseModel getCurrentWorkStatisticsByCompany(UUID companyId) {
         // Confirm that company exists
-        Company company = companyService.getCompanyById(companyId);
+        // TODO: use existsById
+        companyService.getCompanyById(companyId);
 
         Roster remoteRoster = rosterService.getCurrentRemoteRosterByCompany(companyId);
         Set<RosterEmployee> remoteRosterEmployees = remoteRoster.getRosterEmployees();
         int remoteEmployeesCount = remoteRosterEmployees.size();
 
         List<Roster> currentRoster = rosterService.getCurrentRostersByCompany(companyId);
+        Set<RosterEmployee> currentRosterEmployees = currentRoster
+                .stream()
+                .flatMap(roster -> roster.getRosterEmployees().stream())
+                .collect(Collectors.toSet());
+        int totalWorkingEmployeesCount = currentRosterEmployees.size();
 
-        return null;
+        WorkingStatisticResponseModel workingStatisticResponseModel = new WorkingStatisticResponseModel();
+        workingStatisticResponseModel.setCompanyId(companyId);
+        workingStatisticResponseModel.setRemoteCount(remoteEmployeesCount);
+        workingStatisticResponseModel.setOnsiteCount(totalWorkingEmployeesCount - remoteEmployeesCount);
+
+        return workingStatisticResponseModel;
     }
 }
