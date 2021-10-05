bc5c43ecdd6b2935495bd3dcf7e6273c187858e9
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-06 00:19:06 +0800

    Remove autogenerated uuid since this is issued by AWS Cognito
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/model/Employee.java
+++ src/main/java/csd/roster/model/Employee.java
@@ -41,7 +41,7 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private UUID id;
 
