580e3919e0d4e142f05a6fcb9bd80b769c0e8b5b
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-06 01:14:34 +0800

    Add getCurrentEmployeesListByCompany
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/WorkStatisticsService.java
+++ src/main/java/csd/roster/service/WorkStatisticsService.java
@@ -1,11 +1,15 @@
 package csd.roster.service;
 
+import csd.roster.model.Employee;
 import csd.roster.model.RosterEmployee;
 import csd.roster.response_model.WorkingStatisticResponseModel;
 
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 public interface WorkStatisticsService {
     WorkingStatisticResponseModel getCurrentWorkStatisticsByCompany(UUID companyId);
+
+    List<Employee> getCurrentEmployeesListByCompany(UUID companyId);
 }
