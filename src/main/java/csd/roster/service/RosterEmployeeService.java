b5ddc16063da8a3f1aee5bd6df2981b4b7327bd6
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-06 00:39:23 +0800

    Add methods to get remote Roster Employee for the date
    Add methods to get all Roster Employee for the date
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/RosterEmployeeService.java
+++ src/main/java/csd/roster/service/RosterEmployeeService.java
@@ -3,6 +3,8 @@ package csd.roster.service;
 import csd.roster.model.Roster;
 import csd.roster.model.RosterEmployee;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface RosterEmployeeService {
@@ -10,4 +12,6 @@ public interface RosterEmployeeService {
     RosterEmployee getRosterEmployee(UUID rosterId, UUID employeeId);
     void removeRosterEmployee(UUID rosterId, UUID employeeId);
     RosterEmployee updateRosterEmployee(UUID rosterId, UUID employeeId, RosterEmployee rosterEmployee);
+    List<RosterEmployee> findAllRosterEmployeesByCompanyIdAndDate(UUID companyId, LocalDate date);
+    List<RosterEmployee> findRemoteRosterEmployeesByCompanyIdAndDate(UUID companyId, LocalDate date);
 }
