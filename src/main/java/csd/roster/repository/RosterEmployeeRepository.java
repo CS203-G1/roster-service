cde4249085af942397c0345ca8e7098f7ad60cc7
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-06 00:38:45 +0800

    Add custom query to get all remote rosterEmployee for a date
    Add custom query to get all rosterEmployee for a date
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/repository/RosterEmployeeRepository.java
+++ src/main/java/csd/roster/repository/RosterEmployeeRepository.java
@@ -3,12 +3,22 @@ package csd.roster.repository;
 import csd.roster.model.Roster;
 import csd.roster.model.RosterEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface RosterEmployeeRepository extends JpaRepository<RosterEmployee, UUID> {
     Optional<RosterEmployee> findByRosterIdAndEmployeeId(UUID rosterId, UUID employeeId);
+
+    @Query("select re from RosterEmployee re where re.employee.department.company.id = :id and re.roster.date = :date and re.roster.workLocation.name = 'remote'")
+    List<RosterEmployee> findRemoteRosterEmployeesByCompanyIdAndDate(@Param("id") UUID companyId, @Param("date") LocalDate date);
+
+    @Query("select re from RosterEmployee re where re.employee.department.company.id = :id and re.roster.date = :date")
+    List<RosterEmployee> findAllRosterEmployeesByCompanyIdAndDate(@Param("id") UUID companyId, @Param("date") LocalDate date);
 }
