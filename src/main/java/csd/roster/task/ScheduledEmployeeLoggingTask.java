ca77a56c055fcc4a9371dd7a33e663078a3886b1
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-06 21:12:36 +0800

    Add logging
    Add dependencies to EmployeeLog and Employee services
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/task/ScheduledEmployeeLoggingTask.java
+++ src/main/java/csd/roster/task/ScheduledEmployeeLoggingTask.java
@@ -1,4 +1,20 @@
 package csd.roster.task;
 
-public class ScheduledEmployeeLogging {
+import csd.roster.service.EmployeeLogService;
+import csd.roster.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledEmployeeLoggingTask {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledEmployeeLoggingTask.class);
+    private EmployeeService employeeService;
+    private EmployeeLogService employeeLogService;
+
+    @Scheduled
+    public void logEmployeeDetails() {
+//
+    }
 }
