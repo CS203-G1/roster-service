e9d924feac58b23bcc567190f3e6b8c27d1355e7
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-07 01:41:48 +0800

    Use getOnsiteEmployeesListByCompanyAndDate instead
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/WorkStatisticsController.java
+++ src/main/java/csd/roster/controller/WorkStatisticsController.java
@@ -3,6 +3,7 @@ package csd.roster.controller;
 import csd.roster.exception.ResourceNotFoundException;
 import csd.roster.model.Employee;
 import csd.roster.model.WorkLocation;
+import csd.roster.response_model.SummaryResponseModel;
 import csd.roster.response_model.WorkingStatisticResponseModel;
 import csd.roster.service.WorkStatisticsService;
 import org.springframework.http.HttpStatus;
@@ -31,11 +32,16 @@ public class WorkStatisticsController {
 
     @GetMapping("/companies/{companyId}/work-statistics/employees/onsite")
     public List<Employee> getOnsiteEmployeesListByCompany(@PathVariable(value = "companyId") UUID companyId) {
-        return workStatisticsService.getOnsiteEmployeesListByCompany(companyId);
+        return workStatisticsService.getOnsiteEmployeesListByCompanyAndDate(companyId, LocalDate.now());
     }
 
     @GetMapping("/companies/{companyId}/work-statistics/weekly")
     public List<WorkingStatisticResponseModel> getWeeklyWorkStatisticsByCompany(@PathVariable(value = "companyId") UUID companyId) {
         return workStatisticsService.getWorkStatisticsByCompanyAndDateRange(companyId, LocalDate.now().minusDays(6), LocalDate.now());
+    }
+
+    @GetMapping("/companies/{companyId}/summary/employees")
+    public SummaryResponseModel getDailySummaryByCompany(@PathVariable(value = "companyId") UUID companyId) {
+        return workStatisticsService.getSummaryByCompanyIdAndDate(companyId, LocalDate.now());
     }
 }
