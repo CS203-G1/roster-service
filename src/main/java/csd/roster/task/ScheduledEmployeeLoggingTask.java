ce57d368dda04310c44fd5372170ee60417c15be
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-06 22:16:40 +0800

    call getAllEmployees to get a list of existing employees
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/task/ScheduledEmployeeLoggingTask.java
+++ src/main/java/csd/roster/task/ScheduledEmployeeLoggingTask.java
@@ -1,11 +1,14 @@
 package csd.roster.task;
 
+import csd.roster.model.Employee;
 import csd.roster.service.EmployeeLogService;
 import csd.roster.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ScheduledEmployeeLoggingTask {
@@ -16,6 +19,7 @@ public class ScheduledEmployeeLoggingTask {
     @Scheduled
     public void logEmployeeDetails() {
         // Get all employees
+        List<Employee> employees = employeeService.getAllEmployees();
         // Loop through all employees and log one by one asynchronously
     }
 }
