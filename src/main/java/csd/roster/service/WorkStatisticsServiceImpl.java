e9ed3e6b3c5cde79e0c32a30f726367b67729cee
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-07 01:13:05 +0800

    Add logic to calculate the change in employee size
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
+++ src/main/java/csd/roster/service/WorkStatisticsServiceImpl.java
@@ -6,10 +6,7 @@ import csd.roster.response_model.WorkingStatisticResponseModel;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -75,8 +72,44 @@ public class WorkStatisticsServiceImpl implements WorkStatisticsService {
         return workingStatisticResponseModel;
     }
 
+    // TODO: Logic flaw where there might be employees who leave - consider logging them instead
     @Override
     public SummaryResponseModel getSummaryByCompanyIdAndDate(UUID companyId, LocalDate now) {
-        return null;
+        SummaryResponseModel summaryResponseModel = new SummaryResponseModel();
+
+        List<Employee> employees = employeeService.getAllEmployeesByCompanyId(companyId);
+
+        // Considering whether to use database call to get employees who were created before certain date
+        // After some research, consensus seems to be that database query is faster
+        // Since EC2 is well integrated with RDS we will just go ahead with querying the database twice
+
+        List<Employee> employeesBeforePreviousWeek =
+                employeeService.getAllEmployeesByCompanyIdBeforeDate(companyId, now.minusDays(7));
+
+        int change = employees.size() - employeesBeforePreviousWeek.size();
+
+        if (employees.size() == 0) {
+            summaryResponseModel.setEmployeesCount(employees.size());
+
+            if (change == 0) {
+                // if company doesn't have any employees change just put 0
+                summaryResponseModel.setEmployeesCountChange(change);
+            } else {
+                // Example: if now employee size is 0 and last week was 7, change is -700
+                summaryResponseModel.setEmployeesCountChange(change * 100);
+            }
+
+        } else if (employeesBeforePreviousWeek.size() == 0){
+            summaryResponseModel.setEmployeesCount(employees.size());
+
+            // Example: if last week there's 0 employee and today there's 7, change is +700
+            summaryResponseModel.setEmployeesCountChange(change * 100);
+        } else {
+            summaryResponseModel.setEmployeesCount(employees.size());
+
+            int change_rate = (int) ((double) change / employeesBeforePreviousWeek.size() * 100);
+        }
+
+        return summaryResponseModel;
     }
 }
