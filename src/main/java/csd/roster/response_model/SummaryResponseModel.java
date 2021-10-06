46720bc8ee51afbdc5e5af934a0ce125a4a95218
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-07 00:40:06 +0800

    Add attributes for summaryResponseModel as per requested by FrontEnd
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/response_model/SummaryResponseModel.java
+++ src/main/java/csd/roster/response_model/SummaryResponseModel.java
@@ -9,5 +9,19 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class SummaryResponseModel {
+    // Total number of employees for a certain company
+    private int employeesCount;
+    private float employeesCountChange;
 
+    // Total number of employees on leave for a certain company
+    private int leaveCount;
+    private float leaveCountChange;
+
+    // Total number of employees working on site
+    private int onsiteCount;
+    private float onsiteCountChange;
+
+    // Total number of employees contracted with covid 
+    private int covidCount;
+    private float covidCountChange;
 }
