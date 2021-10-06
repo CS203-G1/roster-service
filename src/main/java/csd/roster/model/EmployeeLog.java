7bb74bed83e4966f8ef1413a72deee6a0c7a0fe2
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-06 20:17:06 +0800

    Add lombok annotations
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/model/EmployeeLog.java
+++ src/main/java/csd/roster/model/EmployeeLog.java
@@ -1,4 +1,37 @@
+
 package csd.roster.model;
 
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import csd.roster.enumerator.HealthStatus;
+import csd.roster.enumerator.VaccinationBrand;
+import csd.roster.enumerator.VaccinationStatus;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class EmployeeLog {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
 }
