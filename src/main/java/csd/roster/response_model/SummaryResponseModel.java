a34911a05f2a0bb0617a9c29d10e2ef7a3f0dc88
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-07 00:31:20 +0800

    Add lombok annotations for general getter, setter, all arg and no arg constructor, equal and hashcode
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/response_model/SummaryResponseModel.java
+++ src/main/java/csd/roster/response_model/SummaryResponseModel.java
@@ -1,2 +1,13 @@
-package csd.roster.response_model;public class SummaryResponseModel {
+package csd.roster.response_model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class SummaryResponseModel {
+
 }
