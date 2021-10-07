6e7f163b9a0597981f3c1a581a3b3286553f40ef
Jerald <jeraldssw@gmail.com>
2021-10-07 13:42:53 +0800

    Uncomment auto genereated ID value for Employee

--- src/main/java/csd/roster/model/Employee.java
+++ src/main/java/csd/roster/model/Employee.java
@@ -2,11 +2,23 @@
 package csd.roster.model;
 
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -31,7 +43,7 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class Employee {
     @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private UUID id;
 
