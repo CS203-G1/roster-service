a523f3dfd9887bd45bdea9237a768c63ea1b4f0e
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-10 03:48:58 +0800

    Add PreAuthorize to authorization only users with employer role for work location controller
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/WorkLocationController.java
+++ src/main/java/csd/roster/controller/WorkLocationController.java
@@ -7,12 +7,14 @@ import csd.roster.service.WorkLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@PreAuthorize("hasRole('ROLE_EMPLOYER')")
 public class WorkLocationController {
     final private WorkLocationService workLocationService;
 
