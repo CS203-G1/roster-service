2eda764aea20b896f7d7fdd4114526691318a3d6
Gary Peabody <aikarvin99@gmail.com>
2021-10-14 22:57:18 +0800

    Add test for WorkStatisticService#getSummaryByEmployerIdAndDate
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/test/java/test/csd/roster/service/WorkStatisticsServiceTest.java
+++ src/test/java/test/csd/roster/service/WorkStatisticsServiceTest.java
@@ -1,10 +1,7 @@
 package test.csd.roster.service;
 
 import csd.roster.enumerator.HealthStatus;
-import csd.roster.model.Company;
-import csd.roster.model.Employee;
-import csd.roster.model.Roster;
-import csd.roster.model.RosterEmployee;
+import csd.roster.model.*;
 import csd.roster.response_model.SummaryResponseModel;
 import csd.roster.response_model.WorkingStatisticResponseModel;
 import csd.roster.service.*;
@@ -43,7 +40,7 @@ class WorkStatisticsServiceTest {
     private WorkStatisticsServiceImpl workStatisticsService;
 
     @Test
-    void getOnsiteEmployeesListByCompanyAndDate() {
+    void getOnsiteEmployeesListByCompanyAndDate_returnListOfTwoEmployees() {
         UUID companyId = UUID.randomUUID();
         Company company = new Company();
         company.setId(companyId);
@@ -193,7 +190,7 @@ class WorkStatisticsServiceTest {
     }
 
     @Test
-    void getSummaryByCompanyIdAndDate() {
+    void getSummaryByCompanyIdAndDate_returnSummaryOfCompany() {
         UUID companyId = UUID.randomUUID();
         Company company = new Company();
         company.setId(companyId);
@@ -264,9 +261,115 @@ class WorkStatisticsServiceTest {
 
         assertEquals(7, summaryResponseModel.getEmployeesCount());
         assertEquals(700, summaryResponseModel.getEmployeesCountChange());
+
+        verify(employeeService,times(1)).getAllEmployeesByCompanyIdBeforeDate(companyId, lastWeek);
+        verify(employeeService,times(1)).getEmployeesOnLeaveByCompanyIdAndDate(companyId, lastWeek);
+        verify(employeeService,times(1)).getEmployeesByCompanyIdAndDateAndHealthStatus(companyId, lastWeek, HealthStatus.COVID);
+        verify(rosterEmployeeService, times(1)).findOnsiteRosterEmployeesByCompanyIdAndDate(companyId, lastWeek);
+
+        verify(employeeService,times(1)).getAllEmployeesByCompanyId(companyId);
+        verify(employeeService,times(1)).getEmployeesOnLeaveByCompanyIdAndDate(companyId, now);
+        verify(employeeService,times(1)).getEmployeesByCompanyIdAndDateAndHealthStatus(companyId, now, HealthStatus.COVID);
+        verify(rosterEmployeeService, times(1)).findOnsiteRosterEmployeesByCompanyIdAndDate(companyId, now);
     }
 
     @Test
-    void getSummaryByEmployerIdAndDate() {
+    void getSummaryByEmployerIdAndDate_returnSummaryOfEmployerCompany() {
+        UUID companyId = UUID.randomUUID();
+        Company company = new Company();
+        company.setId(companyId);
+
+        UUID departmentId = UUID.randomUUID();
+        Department department = new Department();
+        department.setId(departmentId);
+        department.setCompany(company);
+
+        UUID employerId = UUID.randomUUID();
+        Employee employer = new Employee();
+        employer.setId(employerId);
+        employer.setDepartment(department);
+
+
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee();
+        Employee employee3 = new Employee();
+        Employee employee4 = new Employee();
+        Employee employee5 = new Employee();
+        Employee employee6 = new Employee();
+        Employee employee7 = new Employee();
+
+        // Mocking the various lists of employees
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        employeeList.add(employee3);
+        employeeList.add(employee4);
+        employeeList.add(employee5);
+        employeeList.add(employee6);
+        employeeList.add(employee7);
+
+        List<Employee> employeesOnLeave = new ArrayList<>();
+        employeesOnLeave.add(employee1);
+
+        List<Employee> employeesWithCovid = new ArrayList<>();
+        employeesWithCovid.add(employee2);
+        employeesWithCovid.add(employee3);
+
+        List<RosterEmployee> onsiteEmployees = new ArrayList<>();
+        RosterEmployee rosterEmployee4 = new RosterEmployee();
+        rosterEmployee4.setEmployee(employee4);
+        RosterEmployee rosterEmployee5 = new RosterEmployee();
+        rosterEmployee4.setEmployee(employee5);
+        RosterEmployee rosterEmployee6 = new RosterEmployee();
+        rosterEmployee4.setEmployee(employee6);
+        RosterEmployee rosterEmployee7 = new RosterEmployee();
+        rosterEmployee4.setEmployee(employee7);
+
+        onsiteEmployees.add(rosterEmployee4);
+        onsiteEmployees.add(rosterEmployee5);
+        onsiteEmployees.add(rosterEmployee6);
+        onsiteEmployees.add(rosterEmployee7);
+
+        LocalDate now = LocalDate.now();
+        LocalDate lastWeek = LocalDate.now().minusDays(7);
+
+        when(employeeService.getEmployee(any(UUID.class))).thenReturn(employer);
+
+        when(employeeService.getAllEmployeesByCompanyIdBeforeDate(any(UUID.class), eq(lastWeek))).thenReturn(new ArrayList<Employee>());
+        when(employeeService.getEmployeesOnLeaveByCompanyIdAndDate(any(UUID.class), eq(lastWeek))).thenReturn(new ArrayList<Employee>());
+        when(employeeService.getEmployeesByCompanyIdAndDateAndHealthStatus(any(UUID.class),eq(lastWeek), eq(HealthStatus.COVID))).thenReturn(new ArrayList<Employee>());
+        when(rosterEmployeeService.findOnsiteRosterEmployeesByCompanyIdAndDate(any(UUID.class), eq(lastWeek))).thenReturn(new ArrayList<>());
+
+        when(employeeService.getAllEmployeesByCompanyId(any(UUID.class))).thenReturn(employeeList);
+        when(employeeService.getEmployeesOnLeaveByCompanyIdAndDate(any(UUID.class), eq(now))).thenReturn(employeesOnLeave);
+        when(employeeService.getEmployeesByCompanyIdAndDateAndHealthStatus(any(UUID.class),eq(now), eq(HealthStatus.COVID))).thenReturn(employeesWithCovid);
+        when(rosterEmployeeService.findOnsiteRosterEmployeesByCompanyIdAndDate(any(UUID.class), eq(now))).thenReturn(onsiteEmployees);
+
+        SummaryResponseModel summaryResponseModel = workStatisticsService.getSummaryByEmployerIdAndDate(employerId, now);
+
+        assertEquals(2, summaryResponseModel.getCovidCount());
+        assertEquals(200, summaryResponseModel.getCovidCountChange());
+
+        assertEquals(4, summaryResponseModel.getOnsiteCount());
+        assertEquals(400, summaryResponseModel.getOnsiteCountChange());
+
+        assertEquals(1, summaryResponseModel.getLeaveCount());
+        assertEquals(100, summaryResponseModel.getLeaveCountChange());
+
+        assertEquals(7, summaryResponseModel.getEmployeesCount());
+        assertEquals(700, summaryResponseModel.getEmployeesCountChange());
+
+
+        verify(employeeService, times(1)).getEmployee(employerId);
+
+        verify(employeeService,times(1)).getAllEmployeesByCompanyIdBeforeDate(companyId, lastWeek);
+        verify(employeeService,times(1)).getEmployeesOnLeaveByCompanyIdAndDate(companyId, lastWeek);
+        verify(employeeService,times(1)).getEmployeesByCompanyIdAndDateAndHealthStatus(companyId, lastWeek, HealthStatus.COVID);
+        verify(rosterEmployeeService, times(1)).findOnsiteRosterEmployeesByCompanyIdAndDate(companyId, lastWeek);
+
+        verify(employeeService,times(1)).getAllEmployeesByCompanyId(companyId);
+        verify(employeeService,times(1)).getEmployeesOnLeaveByCompanyIdAndDate(companyId, now);
+        verify(employeeService,times(1)).getEmployeesByCompanyIdAndDateAndHealthStatus(companyId, now, HealthStatus.COVID);
+        verify(rosterEmployeeService, times(1)).findOnsiteRosterEmployeesByCompanyIdAndDate(companyId, now);
     }
 }
