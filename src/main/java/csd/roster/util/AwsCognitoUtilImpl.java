493b6f96e1236aa3e79d5c92ddad8bc093cb7558
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-18 08:43:36 +0800

    Delete Interface
    Add identity provider using access key and secret key and invoke add user to group
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/util/AwsCognitoUtilImpl.java
+++ src/main/java/csd/roster/util/AwsCognitoUtilImpl.java
@@ -1,28 +1,40 @@
 package csd.roster.util;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClient;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.cognitoidp.model.AdminAddUserToGroupRequest;
 import com.amazonaws.services.cognitoidp.model.AdminAddUserToGroupResult;
-import csd.roster.util.interfaces.AwsCognitoUtil;
 import org.springframework.beans.factory.annotation.Value;
 
-public class AwsCognitoUtilImpl implements AwsCognitoUtil {
+// Use static methods and fields since they will never change - think of Singleton Objects
+public class AwsCognitoUtil {
     @Value("${aws.cognito.userPoolId}")
-    private String userPoolId;
+    private static String userPoolId;
 
     @Value("${aws.access-key}")
-    private String accessKey;
-    
-    @Value("${aws.access-secret}")
-    private String secretKey;
+    private static String accessKey;
 
-    @Override
-    public AdminAddUserToGroupResult addUserToGroup(String userId, String groupName) {
+    @Value("${aws.access-secret}")
+    private static String secretKey;
+
+    public static AdminAddUserToGroupResult addUserToGroup(String userId, String groupName) {
         AdminAddUserToGroupRequest request = new AdminAddUserToGroupRequest();
 
         request.setUserPoolId(userPoolId);
         request.setGroupName(groupName);
         request.setUsername(userId);
 
-        return adminAddUserToGroup;
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCognitoIdentityProvider identityProvider = AWSCognitoIdentityProviderClientBuilder.
+                standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+
+        return identityProvider.adminAddUserToGroup(request);
     }
 }
--- src/main/java/csd/roster/util/interfaces/AwsCognitoUtil.java
+++
@@ -1,8 +0,0 @@
-package csd.roster.util.interfaces;
-
-import com.amazonaws.services.cognitoidp.model.AdminAddUserToGroupRequest;
-import com.amazonaws.services.cognitoidp.model.AdminAddUserToGroupResult;
-
-public interface AwsCognitoUtil {
-    AdminAddUserToGroupResult addUserToGroup(String userId, String groupName);
-}
