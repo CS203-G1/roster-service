f14777a3e57aed8c236f40d92a196531797cd03a
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-18 08:27:12 +0800

    Add addUserToGroup method
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/util/AwsCognitoUtilImpl.java
+++ src/main/java/csd/roster/util/AwsCognitoUtilImpl.java
@@ -1,2 +1,20 @@
-package csd.roster.util.interfaces;public class AwsCognitoUtilImpl {
+package csd.roster.util;
+
+import com.amazonaws.services.cognitoidp.model.AdminAddUserToGroupRequest;
+import com.amazonaws.services.cognitoidp.model.AdminAddUserToGroupResult;
+import csd.roster.util.interfaces.AwsCognitoUtil;
+import org.springframework.beans.factory.annotation.Value;
+
+public class AwsCognitoUtilImpl implements AwsCognitoUtil {
+
+    @Override
+    public AdminAddUserToGroupResult addUserToGroup(String userId, String groupName) {
+        AdminAddUserToGroupRequest request = new AdminAddUserToGroupRequest();
+
+        request.setUserPoolId(userPoolId);
+        request.setGroupName(groupName);
+        request.setUsername(userId);
+
+        return adminAddUserToGroup;
+    }
 }
