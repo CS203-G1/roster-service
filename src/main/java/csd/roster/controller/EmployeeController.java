69413dde84b8ac2bfcdf6143f0ea964580ac3b7c
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-18 09:02:43 +0800

    Reformat controller file
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/EmployeeController.java
+++ src/main/java/csd/roster/controller/EmployeeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @PreAuthorize("hasRole('ROLE_EMPLOYER')")
 public class EmployeeController {
@@ -26,34 +27,34 @@ public class EmployeeController {
     }
 
     @PostMapping("/departments/{departmentId}/employees")
-    public Employee addEmployee (@PathVariable(value = "departmentId") UUID departmentId,
-                                 @Valid @RequestBody Employee employee) {
+    public Employee addEmployee(@PathVariable(value = "departmentId") UUID departmentId,
+                                @Valid @RequestBody Employee employee) {
         return employeeService.addEmployee(departmentId, employee);
     }
 
     @PostMapping("/departments/{departmentId}/employers")
-    public Employee addEmployer (@PathVariable(value = "departmentId") UUID departmentId,
-                                 @Valid @RequestBody Employee employee) {
+    public Employee addEmployer(@PathVariable(value = "departmentId") UUID departmentId,
+                                @Valid @RequestBody Employee employee) {
         return employeeService.addEmployer(departmentId, employee);
     }
 
     @PutMapping("/departments/{departmentId}/employees/{employeeId}")
-    public Employee updateEmployee (@PathVariable(value = "departmentId") UUID departmentId,
-                                    @PathVariable(value = "employeeId") UUID employeeId,
-                                    @Valid @RequestBody Employee employee) {
+    public Employee updateEmployee(@PathVariable(value = "departmentId") UUID departmentId,
+                                   @PathVariable(value = "employeeId") UUID employeeId,
+                                   @Valid @RequestBody Employee employee) {
         return employeeService.updateEmployee(departmentId, employeeId, employee);
     }
 
     @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
     @GetMapping("/departments/{departmentId}/employees/{employeeId}")
-    public Employee getEmployee (@PathVariable(value = "departmentId") UUID departmentId,
-                                    @PathVariable(value = "employeeId") UUID employeeId) {
+    public Employee getEmployee(@PathVariable(value = "departmentId") UUID departmentId,
+                                @PathVariable(value = "employeeId") UUID employeeId) {
         return employeeService.getEmployee(departmentId, employeeId);
     }
 
     @DeleteMapping("/departments/{departmentId}/employees/{employeeId}")
-    public void deleteEmployee (@PathVariable(value = "departmentId") UUID departmentId,
-                                 @PathVariable(value = "employeeId") UUID employeeId) {
+    public void deleteEmployee(@PathVariable(value = "departmentId") UUID departmentId,
+                               @PathVariable(value = "employeeId") UUID employeeId) {
         employeeService.deleteEmployee(departmentId, employeeId);
     }
 
