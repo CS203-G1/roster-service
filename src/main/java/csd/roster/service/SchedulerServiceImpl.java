03eb174129832b558f6391c897cdfa98beeb74a7
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-19 12:39:18 +0800

    Add SchedulerService implementation class
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

---
+++ src/main/java/csd/roster/service/SchedulerServiceImpl.java
@@ -0,0 +1,12 @@
+package csd.roster.service;
+
+import csd.roster.service.interfaces.SchedulerService;
+
+import java.util.UUID;
+
+public class SchedulerServiceImpl implements SchedulerService {
+    @Override
+    public void scheduleRoster(UUID workLocationId) {
+        
+    }
+}
