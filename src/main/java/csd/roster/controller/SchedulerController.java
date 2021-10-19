31618721f1f578d4ce469be681698a5b5a091f2a
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-19 10:00:25 +0800

    Modify controller methdo to return the map of days and list of uuid
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/SchedulerController.java
+++ src/main/java/csd/roster/controller/SchedulerController.java
@@ -1,6 +1,8 @@
 package csd.roster.controller;
 
+import csd.roster.model.Employee;
 import csd.roster.model.RosterEmployee;
+import csd.roster.repository.EmployeeRepository;
 import csd.roster.service.interfaces.RosterEmployeeService;
 import csd.roster.util.Scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +12,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class SchedulerController {
     private Scheduler scheduler;
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public SchedulerController(Scheduler scheduler) {
+    public SchedulerController(Scheduler scheduler, EmployeeRepository employeeRepository) {
         this.scheduler = scheduler;
+        this.employeeRepository = employeeRepository;
     }
 
-    @PostMapping("/schedule")
-    public void schedule() {
-        scheduler.solve();
+    @PostMapping("/schedule/{workLocationId}")
+    public Map<Integer, List<UUID>> schedule(@PathVariable(value = "workLocationId") UUID workLocationId) {
+        List<Employee> li = employeeRepository.findAllByWorkLocationId(workLocationId);
+        List<UUID> li2 = li
+                .stream()
+                .map(e -> e.getId())
+                .collect(Collectors.toList());
+        Map<Integer, List<UUID>> map = scheduler.solve(li);
+
+        return map;
     }
 }
