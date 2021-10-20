98b5c950beeee3465d8a1df8fdd86bc8f2b232c6
Gary Peabody <aikarvin99@gmail.com>
2021-10-20 15:58:24 +0800

    Add column to store imageURL and method to provide key name for S3 bucket
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/main/java/csd/roster/model/ArtRequest.java
+++ src/main/java/csd/roster/model/ArtRequest.java
@@ -1,12 +1,15 @@
 package csd.roster.model;
 
 import csd.roster.enumerator.HealthStatus;
+import csd.roster.exception.EmployeeNotFoundException;
 import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import java.net.URL;
+import java.time.LocalDate;
 
 
 @Entity
@@ -22,9 +25,23 @@ public class ArtRequest extends Request{
     @Column(name = "health_status")
     private HealthStatus healthStatus;
 
-    @Column(name = "image_path")
-    private String imagePath;
+    @Column(name = "image_url")
+    private URL imageUrl;
 
-    @Column(name = "image_file_name")
-    private String imageFileName;
+    public String getFilePath(){
+        if(this.getEmployee() == null ){
+            throw new EmployeeNotFoundException(null);
+        }
+        Employee employee = this.getEmployee();
+        StringBuilder builder = new StringBuilder();
+        builder.append("art_requests/");
+        builder.append("Company_").append(employee.getCompany().getId()).append("/");
+        builder.append("Department_").append(employee.getDepartment().getId()).append("/");
+        builder.append("Employee_").append(employee.getId()).append("/");
+        builder.append(LocalDate.now());
+        return builder.toString();
+
+    }
+
+
 }
