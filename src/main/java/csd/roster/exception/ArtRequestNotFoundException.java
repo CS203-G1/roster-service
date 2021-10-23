add98f8d9064066acef3155086621b30981beb70
Gary Peabody <aikarvin99@gmail.com>
2021-10-23 23:19:51 +0800

    Add ArtRequestNotFoundException
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

---
+++ src/main/java/csd/roster/exception/ArtRequestNotFoundException.java
@@ -0,0 +1,14 @@
+package csd.roster.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.UUID;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ArtRequestNotFoundException extends ResourceNotFoundException{
+    public ArtRequestNotFoundException(UUID id) {
+        super("Could not find ArtRequest " + id);
+    }
+}
