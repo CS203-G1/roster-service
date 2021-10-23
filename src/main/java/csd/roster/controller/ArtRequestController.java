420da4db64d260ab29a519a408571d21d781c18a
Gary Peabody <aikarvin99@gmail.com>
2021-10-24 00:02:48 +0800

    Add authorization to controller methods in ArtRequestController
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/main/java/csd/roster/controller/ArtRequestController.java
+++ src/main/java/csd/roster/controller/ArtRequestController.java
@@ -7,12 +7,14 @@ import csd.roster.model.ArtRequest;
 import csd.roster.service.ArtRequestServiceImpl;
 import csd.roster.service.interfaces.ArtRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
 
+@PreAuthorize("hasRole('ROLE_EMPLOYER')")
 @RestController
 public class ArtRequestController {
     private ArtRequestService artRequestService;
@@ -22,21 +24,25 @@ public class ArtRequestController {
         this.artRequestService = artRequestService;
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYEE')")
     @PostMapping("/employees/{employeeId}/requests/art-request")
     public ArtRequest addArtRequest(@PathVariable(value = "employeeId") UUID employeeId, @RequestParam("file") MultipartFile multipartFile){
         return artRequestService.addArtRequest(employeeId, multipartFile);
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYER')")
     @GetMapping("/employees/{employeeId}/requests/art-request/{requestStatus}")
     public List<ArtRequest> getArtRequestsByEmployeeAndRequestStatus(@PathVariable(value = "employeeId") UUID employeeId, @PathVariable(value = "requestStatus")RequestStatus requestStatus){
         return artRequestService.getArtRequestByEmployeeIdAndRequestStatus(employeeId,requestStatus);
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYER')")
     @GetMapping("/companies/{companyId}/requests/art-request/{requestStatus}")
     public List<ArtRequest> getArtRequestsByCompanyAndRequestStatus(@PathVariable(value = "companyId") UUID companyId, @PathVariable(value = "requestStatus")RequestStatus requestStatus){
         return artRequestService.getArtRequestsByCompanyIdAndApprovalStatus(companyId,requestStatus);
     }
 
+    @PreAuthorize("hasRole('ROLE_EMPLOYER')")
     @PutMapping("/requests/art-request/{artRequestId}")
     public ArtRequest reviewArtRequest(@PathVariable(value = "artRequestId") UUID artRequestId,
                                        @RequestBody ArtRequestReviewHolder artRequestReviewHolder){
