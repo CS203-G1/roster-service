dde5b34f6dbb565b9f0f2975e72d770edcbbc1ba
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-27 12:04:28 +0800

    Add Logger
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/exception/RosterExceptionHandler.java
+++ src/main/java/csd/roster/exception/RosterExceptionHandler.java
@@ -1,6 +1,9 @@
 package csd.roster.exception;
 
+import csd.roster.exception.exceptions.NoOptimalSolutionException;
 import csd.roster.exception.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +12,8 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class RosterExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(RosterExceptionHandler.class);
+
     @ExceptionHandler({ ResourceNotFoundException.class })
     public final ResponseEntity handleNotFoundException(Exception ex, WebRequest request) {
         String errorMessage = ex.getMessage();
