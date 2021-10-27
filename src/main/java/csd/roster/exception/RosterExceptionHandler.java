aacbd1104b631a684830420908971e43a633d3cf
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-27 10:50:51 +0800

    Add exception handler for resourceNotFoundException
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/exception/RosterExceptionHandler.java
+++ src/main/java/csd/roster/exception/RosterExceptionHandler.java
@@ -1,2 +1,18 @@
-package csd.roster.exception;public class RosterExceptionHandler {
+package csd.roster.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class RosterExceptionHandler {
+    @ExceptionHandler({ ResourceNotFoundException.class })
+    public final ResponseEntity handleException(Exception ex, WebRequest request) {
+        String errorMessage = ex.getMessage();
+        return new ResponseEntity(HttpStatus.NOT_FOUND)
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorMessage);
+    }
 }
