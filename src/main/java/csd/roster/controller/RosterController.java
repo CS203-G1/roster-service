f00adc62f8e665716da8085c1b5898b50c57ac3f
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-09 16:50:57 +0800

    controller: RosterController: Remove date param
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/RosterController.java
+++ src/main/java/csd/roster/controller/RosterController.java
@@ -68,8 +68,7 @@ public class RosterController {
     }
 
     @GetMapping("/employee/{employeeId}/rosters/weekly")
-    public List<Roster> getWeeklyRostersByEmployeeIdAndDate(@PathVariable(value = "employeeId")UUID employeeId,
-                                                      @PathVariable(value = "date")LocalDate date) {
+    public List<Roster> getWeeklyRostersByEmployeeIdAndDate(@PathVariable(value = "employeeId")UUID employeeId) {
         return rosterService.getWeeklyRostersByEmployeeId(employeeId);
     }
 }
