9d0b04a81ec8978a80ac924caf125e724ce3c224
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-09 16:13:19 +0800

    response_model: RosterResponseModel: Add response model to store employees array
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

---
+++ src/main/java/csd/roster/response_model/RosterResponseModel.java
@@ -0,0 +1,13 @@
+package csd.roster.response_model;
+
+import csd.roster.model.Employee;
+import csd.roster.model.Roster;
+
+import java.util.List;
+
+public class RosterResponseModel {
+    private Roster roster;
+
+    private List<Employee> employees;
+}
+
