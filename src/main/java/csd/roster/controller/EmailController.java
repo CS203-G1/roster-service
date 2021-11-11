544ffdbd557c0bf76b0108efa82e27293304a3a9
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-12 00:58:47 +0800

    Optimize imports
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/main/java/csd/roster/controller/EmailController.java
+++ src/main/java/csd/roster/controller/EmailController.java
@@ -1,9 +1,9 @@
 package csd.roster.controller;
 
+1import csd.roster.services.service.interfaces.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import csd.roster.services.service.interfaces.EmailService;
 
 @RestController
 public class EmailController {
