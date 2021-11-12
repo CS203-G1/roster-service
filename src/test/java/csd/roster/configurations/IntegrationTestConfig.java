2346e95ed42a5a1d0e01cebfa50f429f358c9d02
Gary Peabody <aikarvin99@gmail.com>
2021-11-12 20:42:49 +0800

    Add Integration Tests for Company
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

--- src/test/java/csd/roster/configurations/IntegrationTestConfig.java
+++ src/test/java/csd/roster/configurations/IntegrationTestConfig.java
@@ -2,13 +2,12 @@ package csd.roster.configurations;
 
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration
+
 @EnableAutoConfiguration
 @ComponentScan
 @PropertySource("application-test.properties")
