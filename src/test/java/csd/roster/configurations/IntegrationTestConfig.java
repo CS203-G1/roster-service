059fa7c858fe2b2d3fddefe119004ad164415d0b
Gary Peabody <aikarvin99@gmail.com>
2021-11-12 19:51:22 +0800

    Added configuration for integration tests
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

---
+++ src/test/java/csd/roster/configurations/IntegrationTestConfig.java
@@ -0,0 +1,17 @@
+package csd.roster.configurations;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@PropertySource("application-test.properties")
+@EnableTransactionManagement
+public class IntegrationTestConfig {
+}
