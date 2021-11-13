2203bb706db375e8c0e49608bcac953cf16cce5a
Gary Peabody <aikarvin99@gmail.com>
2021-11-13 12:18:07 +0800

    Add Integration Test for ArtRequest#GetRequest
    Signed-off-by: Gary Peabody <aikarvin99@gmail.com>

---
+++ src/test/java/csd/roster/integrations/ArtRequestIntegrationTest.java
@@ -0,0 +1,182 @@
+package csd.roster.integrations;
+
+import csd.roster.RosterServiceApplication;
+import csd.roster.configurations.IntegrationTestConfig;
+import csd.roster.domain.enumerator.RequestStatus;
+import csd.roster.domain.exception.exceptions.CompanyNotFoundException;
+import csd.roster.domain.exception.exceptions.DepartmentNotFoundException;
+import csd.roster.domain.exception.exceptions.EmployeeNotFoundException;
+import csd.roster.domain.model.ArtRequest;
+import csd.roster.domain.model.Company;
+import csd.roster.domain.model.Department;
+import csd.roster.domain.model.Employee;
+import csd.roster.repo.repository.ArtRequestRepository;
+import csd.roster.repo.repository.CompanyRepository;
+import csd.roster.repo.repository.DepartmentRepository;
+import csd.roster.repo.util.AwsCognitoUtil;
+import csd.roster.services.service.interfaces.EmployeeService;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import wiremock.org.apache.commons.lang3.RandomStringUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = { RosterServiceApplication.class, IntegrationTestConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class ArtRequestIntegrationTest {
+    @LocalServerPort
+    private int port;
+
+    private final String baseUrl = "http://localhost:";
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    private ArtRequestRepository artRequestRepository;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private AwsCognitoUtil awsCognitoUtil;
+
+    private String accessToken;
+
+    @BeforeEach
+    public void setUp(){
+        this.accessToken = awsCognitoUtil.authenticateAndGetToken();
+
+        Company company = new Company();
+        company.setName("Test Company 1");
+        Company savedCompany = companyRepository.save(company);
+
+        Department department = new Department();
+        department.setCompany(savedCompany);
+        department.setName("Test Department 1");
+        Department savedDepartment = departmentRepository.save(department);
+
+        Employee employee = new Employee();
+        employee.setName("Test Employee 1");
+        employee.setEmail(randomEmail());
+        Employee savedEmployee = employeeService.addEmployee(savedDepartment.getId(), employee);
+
+        ArtRequest artRequest = new ArtRequest();
+        artRequest.setEmployee(savedEmployee);
+        ArtRequest savedArtRequest = artRequestRepository.save(artRequest);
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+        artRequestRepository.deleteAll();
+
+        for(Employee emp : employeeService.getAllEmployees()){
+            //awsCognitoUtil.deleteUser(emp.getId().toString());
+            employeeService.deleteEmployee(emp.getDepartment().getId(), emp.getId());
+        }
+
+        departmentRepository.deleteAll();
+        companyRepository.deleteAll();
+    }
+
+    private String randomEmail(){
+        String prefix = RandomStringUtils.randomAlphabetic(8);
+        return prefix + "@test.com";
+    }
+
+    @Test
+    public void getArtRequestByCompany_CompanyDoesNotExist_Return404() throws URISyntaxException {
+        Company company = new Company();
+        company.setId(UUID.randomUUID());
+
+        URI uri = new URI(baseUrl + port +  "/companies/" + company.getId() + "/requests/art-request/" + RequestStatus.PENDING);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.accessToken);
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+
+        CompanyNotFoundException e = new CompanyNotFoundException(company.getId());
+
+        assertEquals( e.getMessage(), result.getBody());
+        assertEquals(404, result.getStatusCode().value());
+    }
+
+    @Test
+    public void getArtRequestByEmployee_EmployeeDoesNotExist_Return404() throws URISyntaxException {
+        Employee employee = new Employee();
+        employee.setId(UUID.randomUUID());
+
+        URI uri = new URI(baseUrl + port +  "/employees/" + employee.getId() + "/requests/art-request/" + RequestStatus.PENDING);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.accessToken);
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+
+        EmployeeNotFoundException e = new EmployeeNotFoundException(employee.getId());
+
+        assertEquals( e.getMessage(), result.getBody());
+        assertEquals(404, result.getStatusCode().value());
+    }
+
+    @Test
+    public void getArtRequestByCompany_ArtRequestExist_Return200() throws URISyntaxException {
+        Company company = companyRepository.findAll().get(0);
+
+        URI uri = new URI(baseUrl + port +  "/companies/" + company.getId() + "/requests/art-request/" + RequestStatus.PENDING);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.accessToken);
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+
+
+        assertEquals(200, result.getStatusCode().value());
+    }
+
+    @Test
+    public void getArtRequestByEmployee_ArtRequestExist_Return200() throws URISyntaxException {
+        Employee employee = employeeService.getAllEmployees().get(0);
+
+        URI uri = new URI(baseUrl + port +  "/employees/" + employee.getId() + "/requests/art-request/" + RequestStatus.PENDING);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.accessToken);
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+
+
+        assertEquals(200, result.getStatusCode().value());
+    }
+
+
+}
